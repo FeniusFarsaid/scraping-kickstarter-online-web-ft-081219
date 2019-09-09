c0ef85804548cf3926bc9cd99818530cf34177c1
David Ingram <david@ingramaviation.com>
2019-09-09 01:23:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,8 +4,13 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  
+  projects`= {}
+  kickstarter.css
 end
+
+  
+  
  
 create_project_hash
 
